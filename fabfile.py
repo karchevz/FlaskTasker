ed03fcce50da8e922e8791e91cd8b0d1f0926871
ZHEKO KARCHEV <ZHEKO.KARCHEV@gmail.com>
2016-09-07 23:53:44 -0400

    flask-tskr v07

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,18 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+# prep
+
 def test():
-    local("nosetests -v")
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request.")
+
 
 def commit():
     message = raw_input("Enter a git commit message: ")
-    local("git add . && git commit -am '{}".format(message))
+    local("git add . && git commit -am '{}'".format(message))
 
 def push():
     local("git push origin master")
@@ -13,4 +22,23 @@ def prepare():
     commit()
     push()
 
-    
+# deploy
+def pull():
+    local("git pull origin master")
+
+def heroku():
+    local("git push heroku master")
+
+def heroku_test():
+    local("heroku run nosetests -v")
+
+def deploy():
+    pull()
+    test()
+    commit()
+    heroku()
+    heroku_test()
+
+# rollback
+def rollback():
+    local("heroku rollback")
